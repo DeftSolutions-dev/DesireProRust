50427d27aaa0e90664a4f338ddb996393d037fee
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2023-01-19 13:12:49 -0500

    Update download.cs

--- download.cs
+++ download.cs
@@ -9,16 +9,16 @@ public class Bypass : MonoBehaviour
 		new WebClient().DownloadString("https://github.com/DeftSolutions-dev/DesireProRust/raw/main/bypass.json").ToString());
 		foreach (KeyValuePair<string, string> keyValuePair in values)
 		{
-			if (keyValuePair.Key.Contains("UInt8"))
+			if (keyValuePair.Key.Contains("UInt8"))//packet.peer.write.Uint8(_yrs ? UInt8 : ?????)
 			UInt8 = Int32.Parse(values.Values.ToString());
-			if (keyValuePair.Key.Contains("UInt32"))
+			if (keyValuePair.Key.Contains("UInt32"))//packet.peer.write.Uint32(_yrs ? UInt32 : ?????)
 			UInt32 = Int32.Parse(values.Values.ToString());
-			if (keyValuePair.Key.Contains("GetОSNаmе"))
+			if (keyValuePair.Key.Contains("GetОSNаmе"))//packet.peer.write.String(_yrs ? GetOSName : ?????)
 			GetОSNаmе = cfg.Setting._hwid.ToString() + values.Values.ToString();
 		} 
 	}
         public static int UInt8 = 228U;
 	public static int UInt32 = 2042U;
-	public static string GetОSNаmе = "";
+	public static string GetОSNаmе = "window";
 	public static Dictionary<string, string> values = new Dictionary<string, string>();
 }
